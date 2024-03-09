b230da9ccf84145128a7e1bd4b630e9d2650d822
Mingfeng Yu <mfdevworld@gmail.com>
2024-03-09 22:21:31 +0200

    Improve results output
    - justify sizes to the right
    - sort results by size in descending order

--- fzstats/main.py
+++ fzstats/main.py
@@ -50,19 +50,36 @@ def get_children_sizes(folder_path: str = ".") -> list[PathSize]:
         elif os.path.isdir(child_path):
             size = get_folder_size(child_path)
             children_with_sizes.append(PathSize(child, "folder", size))
+    # by default sort results by size in descending order
+    children_with_sizes.sort(key=lambda item: item.size, reverse=True)
     return children_with_sizes
+
+
+def format_size(size: int) -> str:
+    one_kb = 1024
+    one_mb = 1024**2
+    one_gb = 1024**3
+    if size > one_gb:
+        return f"{size / one_gb:.1f}G"
+    if size > one_mb:
+        return f"{size / one_mb:.1f}M"
+    if size > one_kb:
+        return f"{size / one_kb:.1f}K"
+    return f"{size}B"
 
 
 def main(folder_path: Annotated[str, typer.Argument()] = "."):
     children_with_sizes = get_children_sizes(folder_path)
-    table = Table("Name", "Type", "Size")
+    table = Table()
+    table.add_column("Name")
+    table.add_column("Type")
+    table.add_column("Size", justify="right")
     for item in children_with_sizes:
-        table.add_row(item.name, item.type, f"{item.size}B")
-    
+        table.add_row(item.name, item.type, format_size(item.size))
+
     console = Console()
     console.print(table)
 
 
 if __name__ == "__main__":
     typer.run(main)
-    
