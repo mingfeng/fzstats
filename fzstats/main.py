239f513ea00d72dad0630ea8ab5290091a70068d
Mingfeng Yu <mfdevworld@gmail.com>
2024-03-10 21:52:45 +0200

    Replace reverse option with sort direction

--- fzstats/main.py
+++ fzstats/main.py
@@ -23,6 +23,11 @@ class SortBy(Enum):
     SIZE = "size"
 
 
+class SortDirection(Enum):
+    DESC = "desc"
+    ASC = "asc"
+
+
 def get_folder_size(folder_path="."):
     """Get folder size
 
@@ -44,7 +49,7 @@ def get_folder_size(folder_path="."):
 def get_children_sizes(
     folder_path: str = ".",
     sort_by: str = "size",
-    reverse: bool = False,
+    sort_dir: Literal["asc", "desc"] = "desc",
     limit: int = None,
 ) -> list[PathSize]:
     """Get folder children sizes
@@ -52,8 +57,8 @@ def get_children_sizes(
     Args:
         folder_path (str, optional): The path to the folder. Defaults to ".".
         sort_by (str, optional): Sort results by. Defaults to "size".
-        reverse (bool, optional): Sort results in reverse order. Defaults to False.
-        limit (int, optional): limit results by. Defaults to None.
+        sort_dir (str, optional): Sort direction. Defaults to "desc".
+        limit (int, optional): limit results by. Defaults to "None".
 
     Returns:
         list[PathSize]: folder children with their sizes
@@ -68,8 +73,10 @@ def get_children_sizes(
         elif os.path.isdir(child_path):
             size = get_folder_size(child_path)
             children_with_sizes.append(PathSize(child, "folder", size))
-    # by default sort results by size in descending order
+
+    reverse = sort_dir == "desc"
     children_with_sizes.sort(key=lambda item: getattr(item, sort_by), reverse=reverse)
+
     if limit:
         children_with_sizes = children_with_sizes[0:limit]
     return children_with_sizes
@@ -98,22 +105,25 @@ def limit_callback(value: int):
 def main(
     folder_path: Annotated[str, typer.Argument(help="The path to the folder.")] = ".",
     sort_by: Annotated[
-        SortBy, typer.Option("--sort", "-s", help="Sort results by.")
+        SortBy,
+        typer.Option("--sort", "-s", help="Sort the results by a specific attribute."),
     ] = "size",
-    reverse: Annotated[
-        bool, typer.Option("--reverse", "-r", help="Show results in reverse order.")
-    ] = False,
+    sort_dir: Annotated[
+        SortDirection, typer.Option("--dir", "-d", help="Sort direction.")
+    ] = "desc",
     limit: Annotated[
         int,
         typer.Option(
             "--limit",
             "-l",
             callback=limit_callback,
-            help="Limit results by.",
+            help="Limit the results to a given number of rows.",
         ),
     ] = None,
 ):
-    children_with_sizes = get_children_sizes(folder_path, sort_by.value, reverse, limit)
+    children_with_sizes = get_children_sizes(
+        folder_path, sort_by.value, sort_dir.value, limit
+    )
     table = Table(
         "Name",
         Column("Size", justify="right", style="green"),
